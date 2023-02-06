cafd58afd71b5f54d3cb58dc2d698a05eb58cc10
narsingh <narsingh650@gmail.com>
2023-02-06 21:54:38 +0530

    initial-commit

---
+++ src/main/java/com/serviceworker/ServiceWorkerApplication.java
@@ -0,0 +1,13 @@
+package com.serviceworker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceWorkerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceWorkerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/serviceworker/ServiceWorkerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.serviceworker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceWorkerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
