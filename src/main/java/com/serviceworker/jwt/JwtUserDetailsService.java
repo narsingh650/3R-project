acb360751129e76ba5b67d06e305990a53435caf
Pushkar Bisht <pushkar@pushkar.bisht>
2023-02-13 22:05:39 +0530

    code rearrange

---
+++ src/main/java/com/serviceworker/jwt/JwtUserDetailsService.java
@@ -0,0 +1,41 @@
+package com.serviceworker.jwt;
+
+import com.serviceworker.dto.UserDto;
+import com.serviceworker.model.User;
+import com.serviceworker.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+
+
+@Service
+public class JwtUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder bcryptEncoder;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username);
+        if (user == null) {
+            throw new UsernameNotFoundException("User not found with username: " + username);
+        }
+        return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(),
+                new ArrayList<>());
+    }
+
+    public User save(UserDto userDto) {
+        User newUser = new User();
+        newUser.setUsername(userDto.getUsername());
+        newUser.setPassword(bcryptEncoder.encode(userDto.getPassword()));
+        return userRepository.save(newUser);
+    }
+}
--- src/main/java/com/serviceworker/repository/UserRepository.java
+++ src/main/java/com/serviceworker/repository/UserRepository.java
@@ -1,4 +1,15 @@
 package com.serviceworker.repository;
 
-public interface UserRepository {
+import com.serviceworker.model.User;
+import org.springframework.data.repository.CrudRepository;
+
+import org.springframework.stereotype.Repository;
+
+
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Integer> {
+	
+	User findByUsername(String username);
+	
 }
